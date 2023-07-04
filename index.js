bb3569f93d0e9d193047e3a633c7e6abe14fa6f7
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-07-05 01:50:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+let lib = 'https://gutendex.com/books';
+let fetcher = (page = 1, url = `${lib}?page=${page}&mime_type=image`) => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(obj => {
+        ulSetter(obj.results);
+        buttonToggle();
+      });
+  };
+  
